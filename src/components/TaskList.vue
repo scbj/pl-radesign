577eb84fdc7062148233579a9cbd0c21960586fa
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-05-23 16:44:46 +0200

    Refactoring task list in its <TaskList> component

---
+++ src/components/TaskList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="task-list">
+    <li v-for="(task, index) in tasks" :key="index">
+      <ProjectTask
+        class="task"
+        :task="task"
+        :active="index === 0"
+        owner="Leader" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import ProjectTask from '@/components/ProjectTask.vue'
+
+export default {
+  components: {
+    ProjectTask
+  },
+
+  props: {
+    tasks: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.task-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.task {
+  margin: 1rem;
+}
+</style>
--- src/views/TeamProject.vue
+++ src/views/TeamProject.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="team-project">
-    <ul class="tasks">
-      <li v-for="(task, index) in tasks" :key="index">
-        <ProjectTask :task="task" :active="index === 0" owner="Leader" />
-      </li>
-    </ul>
-    <BaseCard>Project</BaseCard>
+    <TaskList :tasks="tasks" />
   </div>
 </template>
 
 <script>
-import ProjectTask from '@/components/ProjectTask.vue'
+import TaskList from '@/components/TaskList.vue'
 
 import { tasks } from '@/assets/data/team-project'
 
 export default {
   components: {
-    ProjectTask
+    TaskList
   },
 
   computed: {
@@ -28,15 +23,5 @@ export default {
 <style lang="scss" scoped>
 .team-project {
   padding: 2rem;
-}
-
-.tasks {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-
-.base-card {
-  margin: 1rem;
 }
 </style>
