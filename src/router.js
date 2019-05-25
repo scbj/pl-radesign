b7ab6c721191fcb908ab5f473e662c181ef70709
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-05-25 02:37:56 +0200

    Async loading components and document title

--- src/router.js
+++ src/router.js
@@ -1,47 +1,94 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import TeamCollect from './views/TeamCollect.vue'
-import TeamData from './views/TeamData.vue'
-import TeamProject from './views/TeamProject.vue'
 
 Vue.use(Router)
 
-export default new Router({
+export const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      redirect: '/home'
+      redirect: { name: 'project' }
     },
     {
       path: '/home',
       name: 'home',
-      component: Home
+      component: () => import('@/views/Home.vue'),
+      meta: {
+        title: 'Home - Radesign P.L.'
+      }
     },
     {
       path: '/collect',
       name: 'collect',
-      component: TeamCollect
+      component: () => import('@/views/TeamCollect.vue'),
+      meta: {
+        title: 'Team collect - Radesign P.L.'
+      }
     },
     {
       path: '/data',
       name: 'data',
-      component: TeamData
+      component: () => import('@/views/TeamData.vue'),
+      meta: {
+        title: 'Team data - Radesign P.L.'
+      }
     },
     {
       path: '/project',
       name: 'project',
-      component: TeamProject
+      component: () => import('@/views/TeamProject.vue'),
+      meta: {
+        title: 'Team project - Radesign P.L.'
+      }
+    },
+    {
+      path: '/skill/:tag',
+      name: 'skill',
+      component: () => import('@/views/Skill.vue'),
+      meta: {
+        title: 'Skill - Radesign P.L.'
+      }
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ]
 })
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el))
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next()
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta')
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+
+    // We use this to track which meta tags we create, so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '')
+
+    return tag
+  })
+  // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag))
+
+  next()
+})
+
+export default router
